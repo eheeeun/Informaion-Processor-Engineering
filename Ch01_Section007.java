2612790c617231ac1680f7d6e44640c79df71a81
eheeeun <38809474+eheeeun@users.noreply.github.com>
2020-05-11 20:03:18 +0900

    Update Ch01_Section007.java

--- Ch01_Section007.java
+++ Ch01_Section007.java
@@ -3,7 +3,7 @@ public class Problem{
     int [][]a = {{11, 12, 13, 14}, {21, 22, 23, 24}};
     int hap = 0;
     for(int i[] : a){     // a의 행의 갯수만큼 반복 여기선 2번
-      for(int j : i)      // i배열의 요소 수만큼 작어 반복
+      for(int j : i)      // i배열의 요소 수만큼 작 반복
         hap = hap + j;
     }
     System.out.println("%d", hap);
